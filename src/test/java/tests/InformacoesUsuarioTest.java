88f77be1d4aed5fb6c820801ac1c4f984c7c638c
larissademiranda <larissademiranda@hotmail.com>
2018-09-24 18:42:02 -0300

    ok 2

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -11,23 +11,31 @@ public class InformacoesUsuarioTest {
     @Test
     public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
 
-        //Abrindo o navegador
-        System.setProperty("webdriver.chrome.driver", "c:\\Selenium\\Drivers\\chromedriver.exe");
-        WebDriver navegador = new ChromeDriver();
+        try {
+            //Abrindo o navegador
+            System.setProperty("webdriver.chrome.driver", "/Users/larissademiranda/Projects/chromedriver/chromedriver");
+            WebDriver navegador = new ChromeDriver();
 
-        //Navegando para a página do Google!
-        navegador.get("https://www.bing.com/");
+            //Navegando para a página do Google!
+            navegador.get("https://www.bing.com/");
 
-        // Clicar no campo que possui o id "sb_form_q"
-       navegador.findElement(By.id("sb_form_q")).click();
+            // Clicar no campo que possui o id "sb_form_q"
+            navegador.findElement(By.id("sb_form_q")).click();
 
-        // Digitar no campo que possui o id "sb_form_q" o texto "teste"
-        navegador.findElement(By.id("sb_form_q")).sendKeys("teste de velocidade");
+            // Digitar no campo que possui o id "sb_form_q" o texto "teste"
+            navegador.findElement(By.id("sb_form_q")).sendKeys("teste de velocidade");
 
-        // Clicar no botão "Pesquisar"
-        ((ChromeDriver) navegador).findElementById("sb_form_go").click();
+            // Clicar no botão "Pesquisar"
+            ((ChromeDriver) navegador).findElementById("sb_form_go").click();
 
+            //clicar no link que possui texto "www.minhaconexao.com.br"
+            ((ChromeDriver) navegador).findElementByLinkText("www.minhaconexao.com.br")
 
+;
+        }atch (Exception e){
+            e.printStackTrace();
+            fail();
+        }
 
         //fechar o navegador
 
