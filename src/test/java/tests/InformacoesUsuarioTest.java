97543abf5926a7d6853c9c51f1b26d7fc8103a4c
c1401657 <larissademiranda@hotmail.com>
2018-10-17 16:36:39 -0300

    Inclusão de comentários

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -78,7 +80,12 @@ public class InformacoesUsuarioTest {
 
             //Clicar no botão "Get New User"
             ((ChromeDriver) navegador).findElementById("Save").click();
+            WebDriver driver = new ChromeDriver();
+            Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                    .withTimeout(10, TimeUnit.SECONDS);
 
+            //Clicar no botão "Get New User"
+            ((ChromeDriver) navegador).findElementById("Save").click();
 
 
 
